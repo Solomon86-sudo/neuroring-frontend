372d5fced9399251954cf029c17b3da1f35fe0ad
Solomon <solomon6g8ed2@gmail.com>
2026-05-22 21:13:06 +0300

    clean next config syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* здесь можно оставить пустое место или настройки, если они были */
 };
 
 export default nextConfig;
